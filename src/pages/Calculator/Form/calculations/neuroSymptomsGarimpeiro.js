e11f409ba059f3b0f72bbbaf3ba601d46e626ffc
Keven Jesus <falecom@keven.com.br>
2021-05-12 10:10:04 -0300

    Alteração na TxPrevalence no impacto de neuro

--- src/pages/Calculator/Form/calculations/neuroSymptomsGarimpeiro.js
+++ src/pages/Calculator/Form/calculations/neuroSymptomsGarimpeiro.js
@@ -1,11 +1,12 @@
 import { FERRY, MONTHS_OF_MINING, PIT, YEARS_OF_MINING } from "../consts";
 
 const neuroSymptomsGarimpeiro = (likeMining, typeValueLikeMining, valueLikeMining, txPrevalence, gold) => {
+
+    
     
     const DALY1Cost = 103599;
     const weightNeuroDisabilityGoldminers = 0.368;
     const amountOfGoldminersYear = 150.45;
-    const neuroGoldminersProblems = 0.72;
     const neuroTreatmentCostPerGoldMiner = 2244;
     const gramGoldByYearWell = 23700;
     const goldenGrassPerMonthFerry = 302;
@@ -13,7 +14,7 @@ const neuroSymptomsGarimpeiro = (likeMining, typeValueLikeMining, valueLikeMinin
     if (likeMining === PIT && typeValueLikeMining === YEARS_OF_MINING) { //Input Anos de garimpo
         const goldenGrass = gramGoldByYearWell * valueLikeMining;
         const qtdTotalGoldMiners = goldenGrass / amountOfGoldminersYear;
-        const qtdOfMinersAffected = neuroGoldminersProblems * qtdTotalGoldMiners;
+        const qtdOfMinersAffected = txPrevalence * qtdTotalGoldMiners;
         const neuroGoldMinersTreatmentCost = neuroTreatmentCostPerGoldMiner * qtdOfMinersAffected;
 	
         const weightNeuroDisabilityGoldminersQtdGoldDiggers = weightNeuroDisabilityGoldminers * qtdTotalGoldMiners;
@@ -25,7 +26,7 @@ const neuroSymptomsGarimpeiro = (likeMining, typeValueLikeMining, valueLikeMinin
     }else if (likeMining === FERRY && typeValueLikeMining === MONTHS_OF_MINING) { //Input Meses de garimpo
         const goldenGrass = goldenGrassPerMonthFerry * valueLikeMining;
         const tonumberOfGoldMiners = goldenGrass / amountOfGoldminersYear;
-        const numberOfMinersAffected = neuroGoldminersProblems * tonumberOfGoldMiners;
+        const numberOfMinersAffected = txPrevalence * tonumberOfGoldMiners;
         const neuroGoldMinersTreatmentCost = neuroTreatmentCostPerGoldMiner * numberOfMinersAffected;
 
         const weightNeuroDisabilityGoldminers_QtdGarimpeiros = weightNeuroDisabilityGoldminers  * tonumberOfGoldMiners;
@@ -36,7 +37,7 @@ const neuroSymptomsGarimpeiro = (likeMining, typeValueLikeMining, valueLikeMinin
 
     }else {
         const qtdTotalGoldMiners = gold / amountOfGoldminersYear;
-        const qtdOfMinersAffected = qtdTotalGoldMiners * neuroGoldminersProblems;
+        const qtdOfMinersAffected = qtdTotalGoldMiners * txPrevalence;
         const neuroGoldMinersTreatmentCost = qtdOfMinersAffected * neuroTreatmentCostPerGoldMiner;
 	
         const weightNeuroDisabilityGoldminersQtdGoldDiggers = weightNeuroDisabilityGoldminers * qtdTotalGoldMiners;
