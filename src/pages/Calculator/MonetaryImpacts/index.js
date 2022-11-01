640bcc22e6751d87c592e20ded4ced82e548515b
Keven Jesus <falecom@keven.com.br>
2022-11-01 01:34:38 -0300

    ajustes

--- src/pages/Calculator/MonetaryImpacts/index.js
+++ src/pages/Calculator/MonetaryImpacts/index.js
@@ -517,10 +517,14 @@ const MonetaryImpacts = () => {
         const graphics_total = document.getElementById('graphics_total');
         const totalMonay = document.getElementById('totalMoney');
         const tablenotMonetary = document.getElementById('table-notMonetary');
+        const headlineNotMonetary = document.getElementById('headline-notMonetary');
+        tablenotMonetary.firstElementChild.style.maxWidth = '73%';
+        headlineNotMonetary.style.fontSize = '23px';
+        
         const canvasGraphics_resume = await html2canvas(graphics_resume, { scale: 0.55 })
         const canvasGraphics_total = await html2canvas(graphics_total, { scale: 0.53 })
         const canvasTotalMoney = await html2canvas(totalMonay, { scale: 0.8 })
-        const canvasToNotMonetary = await html2canvas(tablenotMonetary, { scale: 0.52})
+        const canvasToNotMonetary = await html2canvas(tablenotMonetary, {scale: 0.7})
         
         var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         var today  = new Date();
@@ -542,13 +546,15 @@ const MonetaryImpacts = () => {
         pdf.text('https://calculadora.conservation-strategy.org', 146, 287, { align: 'left' })
         pdf.addImage(canvasTotalMoney.toDataURL('image/png'), 'JPEG', 7, 10);
         pdf.addImage(canvasGraphics_resume.toDataURL('image/png'), 'JPEG', 10, 55);
-        pdf.addImage(canvasToNotMonetary.toDataURL('image/png'), 'JPEG', 7, 135);
+        pdf.addImage(canvasToNotMonetary.toDataURL('image/png'), 'JPEG', 7, 137);
         pdf.addPage('a4', 'p')
         
         pdf.addImage(canvasGraphics_total.toDataURL('image/png'), 'JPEG', 7, 10);
         pdf.text(footer, 88, 287, { align: 'right' })
         pdf.text('https://calculadora.conservation-strategy.org', 146, 287, { align: 'left' })
         pdf.save("CSF-report.pdf");
+        tablenotMonetary.firstElementChild.removeAttribute("style")
+        headlineNotMonetary.removeAttribute("style")
 
     }, [isBrazil])
     
@@ -625,13 +631,13 @@ const MonetaryImpacts = () => {
                 <br />
                <Row id="table-notMonetary">
                     <Col xs={12}>
-                        <h2>Impactos não monetários</h2>
+                        <h2 id="headline-notMonetary">Impactos não monetários</h2>
                         <br />
                         <S.TableResponsive>
                         <S.Table>
                             <thead>
                                 <tr>
-                                    <S.Th width="800px">Tipo de impacto</S.Th>
+                                    <S.Th width="700px">Tipo de impacto</S.Th>
                                     <S.Th>Resultado</S.Th>
                                 </tr>
                             </thead>
